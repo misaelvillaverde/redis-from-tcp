676dd7d976bc2ce3149f69cffd361f26ba6c496f
misaelvillaverde <misaelvilla10@gmail.com>
2024-05-20 18:45:58 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-use std::net::TcpListener;
+use std::{io::Write, net::TcpListener};
+
+const CLRF: &str = "\r\n";
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let buffer = format!("+PONG{CLRF}");
+
+                match _stream.write(buffer.as_bytes()) {
+                    Ok(_) => (),
+                    Err(e) => println!("error writing to stream: {}", e),
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
