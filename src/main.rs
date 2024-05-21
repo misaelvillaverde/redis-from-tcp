e9c2d183db132e689a61a569b22f9e0070b924c1
misaelvillaverde <misaelvilla10@gmail.com>
2024-05-20 19:41:34 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,37 +5,40 @@ use std::{
 
 const CLRF: &str = "\r\n";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(mut _stream) => loop {
-                let mut incoming = [0; 1024];
+        tokio::spawn(async {
+            match stream {
+                Ok(mut _stream) => loop {
+                    let mut incoming = [0; 1024];
 
-                match _stream.read(&mut incoming) {
-                    Ok(n) => {
-                        if n <= 0 {
-                            break;
+                    match _stream.read(&mut incoming) {
+                        Ok(n) => {
+                            if n <= 0 {
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            println!("error reading from stream: {}", e);
                         }
                     }
-                    Err(e) => {
-                        println!("error reading from stream: {}", e);
-                    }
-                }
 
-                let buffer = format!("+PONG{CLRF}");
+                    let buffer = format!("+PONG{CLRF}");
 
-                match _stream.write(buffer.as_bytes()) {
-                    Ok(_) => (),
-                    Err(e) => {
-                        println!("error writing to stream: {}", e);
+                    match _stream.write(buffer.as_bytes()) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            println!("error writing to stream: {}", e);
+                        }
                     }
+                },
+                Err(e) => {
+                    println!("error: {}", e);
                 }
-            },
-            Err(e) => {
-                println!("error: {}", e);
             }
-        }
+        });
     }
 }
